fa714f92786329524ed896620ba846af2d9628e3
shiiiifa <shifa.gabbey@ashesi.edu.gh>
2024-11-12 22:17:29 +0000

    new class "AdditionalCourses" for students who want to take extra courses outside their major

---
+++ ASIS/AdditionalCourses.java
@@ -0,0 +1,8 @@
+package ASIS;
+
+public class AdditionalCourses {
+    //This class is for students who want to take additional courses
+    //There should be a condition here.
+    // If the person's GPA is not within a particular threshold,
+    // they can't take the course. Also, the number of credits should not exceed 5.5
+}
