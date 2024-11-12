553a2527e554b083c7f9840dbb0bc3055f2b6d3c
shiiiifa <shifa.gabbey@ashesi.edu.gh>
2024-11-12 22:30:10 +0000

    new methods added to the getCourseInfo interface

--- ASIS/getCourseInfo.java
+++ ASIS/getCourseInfo.java
@@ -1,5 +1,10 @@
 package ASIS;
 
 public interface getCourseInfo {
-    //Hello again
+    String getCourseID();
+    String getCourseName();
+    boolean isCourseMet();
+    void setCourseID(String courseID);
+    void setCourseName(String courseName);
+    void setCourseMet(boolean courseMet);
 }
