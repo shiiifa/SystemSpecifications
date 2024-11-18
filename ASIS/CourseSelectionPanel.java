605e9d7533e0f6e9c3af8727720c6201c9d05281
shiiiifa <shifa.gabbey@ashesi.edu.gh>
2024-11-18 14:26:52 +0000

    courses added

--- ASIS/CourseSelectionPanel.java
+++ ASIS/CourseSelectionPanel.java
@@ -67,34 +67,47 @@ public class CourseSelectionPanel {
 
 
         year2Courses.add("SOAN234: African Music and the Art Music Scen");
+        year2Courses.add("MATH000: Principles of Economics");
         year2Courses.add("CS212: Computer Programming for CS");
         year2Courses.add("CS221: Discrete Structures and Theory");
-        year2Courses.add("");
-        year2Courses.add("");
-        year2Courses.add("");
-        year2Courses.add("");
+        year2Courses.add("STAT151: Statistics with Probability ");
+        year2Courses.add("SOAN211: Leadership Seminar 2");
+        year2Courses.add("CS221: Discrete Structures & Theory ");
+        year2Courses.add("CS213: Object Oriented Programming");
+        year2Courses.add("MATH000: Linear Algebra");
+        year2Courses.add("SOAN311: Leadership Seminar 3");
+        year2Courses.add("CS000: Data Structures & Algorithms");
+        year2Courses.add("CS001: Database Systems");
+        year2Courses.add("CS002: Introduction to Artificial Intelligence");
 
 
-        year3Courses.add("SOAN322: African Cultural Institutions");
+        year3Courses.add("CS000: Algorithm Design & Analysis");
         year3Courses.add("CS341: Web Technologies");
         year3Courses.add("ENGR311: System Dynamics");
         year3Courses.add("EE32O: Signals and Systems");
-        year3Courses.add("");
-        year3Courses.add("");
+        year3Courses.add("CS001: Intermediate Computer Programming ");
+        year3Courses.add("CS002: Systems Fundamentals");
+        year3Courses.add("SOAN411: Leadership Seminar 4");
+        year3Courses.add("SOAN325: Research Methods");
+        year3Courses.add("CS003: Software Engineering");
+        year3Courses.add("CS004: Computer Organization & Architecture ");
+
 
 
         year4Courses.add("CS462: Cloud Computing");
         year4Courses.add("CS331: Computer Organization and Architecture");
         year4Courses.add("CS442: E-Commerce");
         year4Courses.add("CS432: Networks and Data Communication");
+        year4Courses.add("BUSA000: Finance for Non-Finance Managers ");
         year4Courses.add("CE451: Embedded Systems");
         year4Courses.add("CS433: Operating Systems");
         year4Courses.add("CS452: Machine Learning");
         year4Courses.add("CS458: Internet of Things");
+        year4Courses.add("CS000: Computer Organization & Architecture");
         year4Courses.add("ENGR401: Engineering Capstone Seminar");
         year4Courses.add("IS451: Information and Systems Security");
         year4Courses.add("SOAN441: Leadership Seminar IV for Engineers: Leadership as Service");
-
+        year4Courses.add("BUSA001: Thesis 2/Applied Project/Entrepreneurship");
 
 
         additionalCourses.add("CS491: Undergraduate Thesis I (CS)");
